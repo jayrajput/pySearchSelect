460f2b412cde0930daf77274aa0bd85ef566ea5d
Jay Rajput <jayrajput@gmail.com>
2013-02-23 18:06:10 -0500

    Added views example. Removed py extension from pySearchSelect

---
+++ examples/README.views.md
@@ -0,0 +1,28 @@
+viewSearchSelect
+================
+
+bash script to interactively search and select cleartool views.
+
+I have written this script to interactively select my clearcase views.  This
+script passed a list of views to the pySearchSelect command to show a full
+screen curses based menu to the user to search and select a view. The list
+contains the most recently used views followed by others. This script keep track
+of the most recently views.
+
+Responsibility of this script:
+- Manage the list of all views in allView file.
+- Manage the list of MRU views in mruView file..
+- Periodically update the above two list.
+
+I have created allView and mruView needed by this script these files under
+~/.viewSearchSelect on my development machine.
+
+Responsibility of pySearchSelect is to provide a GUI for search and selection
+of views.
+
+Cleartool terminology/commands:
+- cleartool setview <view> : Set the given view.
+- cleartool lsview -short  : Show the short name for all the views on the system.
+
+
+
---
+++ examples/views.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+myDir=~/.viewSearchSelect
+selViewFile=$myDir/selViewFile
+mruViewFile=$myDir/mruViewFile
+allViewFile=$myDir/allViewFile
+
+if [ ! -d $myDir ]; then mkdir $myDir; fi
+touch $selViewFile;
+touch $mruViewFile;
+touch $allViewFile;
+
+CT_SETVIEW="cleartool setview "
+CT_LSVIEW="cleartool lsview --short "
+CT_SETVIEW="echo"
+CT_LSVIEW="cat /tmp/views"
+
+function myview()
+{
+    # clear any previous selection.
+    cat /dev/null > $selViewFile;
+    # pySearchSelect shall be in your $PATH
+    pySearchSelect -f $selViewFile -l $(cat $mruViewFile) $(cat $allViewFile)
+    # if file has some contents
+    if [[ -s $selViewFile ]]; then
+        pushViewToMruList $(cat $selViewFile);
+        $CT_SETVIEW $(cat $selViewFile);
+    fi
+}
+
+function pushViewToMruList()
+{
+    local view=$1;
+    # remove if exist
+    sed -i "/^$view\$/d" $mruViewFile;
+    # add to the top of the file
+    if [[ ! -s $mruViewFile ]]; then
+        echo $view > $mruViewFile;
+    else
+        sed -i "1i $view" $mruViewFile;
+    fi
+}
+
+# call this to remove dead view from mru list.
+function updateMruViewList()
+{
+    # remove any dead view
+    # for the terse grep statement see 
+    # http://blog.codevariety.com/2012/03/07/shell-subtract-lines-of-one-file-from-another-file/
+    for deadView in $(grep -Fvx --file=$allViewFile $mruViewFile); do
+        sed -i "/$deadView/d" $mruViewFile;
+    done
+}
+
+# call this to add new views and remove dead views from the list.
+function updateAllViewList()
+{
+    $CT_LSVIEW 2>&1 > $allViewFile
+    # TESTING cat /tmp/views 2>&1 > $allViewFile
+    # remove any view which already exist in the mruViewFile
+    for view in $(cat $mruViewFile); do
+        sed -i "/$view/d" $allViewFile;
+    done
+}
+
+# Test helper
+function viewdebug()
+{
+    for i in $selViewFile $mruViewFile $allViewFile; do
+        echo "cat $i";
+        cat $i;
+        echo ""; # new line character
+    done
+}
+
+# helpful non-interactive functions.
+function lastview()
+{
+    local lastView=$(head -1 $mruViewFile)
+    $CT_SETVIEW $lastView
+}
+
+function mruview
+{
+    PS3="Your choice: "
+    export VIEW=""
+    # running all view to the select will be bad, so just stick with most
+    # recently used.
+    select VIEW in $(cat $mruViewFile)
+    do
+        case $VIEW in
+        * ) break ;;
+        quit ) return ;;
+        esac
+    done
+    pushViewToMruList $VIEW
+    $CT_SETVIEW $VIEW
+}
